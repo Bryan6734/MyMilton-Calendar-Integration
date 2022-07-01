5660873f6fe66d29327bce536c49daffd23e1812
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2022-07-01 16:06:34 -0400

    Cleaned up code & set-up for creating two calendar lists

--- gcal.py
+++ gcal.py
@@ -11,7 +11,8 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-CALENDER_ID = 'ref72r792mf33o1ts1g75abucc@group.calendar.google.com'
+BLUE_CALENDER_ID = 'ref72r792mf33o1ts1g75abucc@group.calendar.google.com'
+ORANGE_CALENDER_ID = 'ssgcd203phkqi7goarojd7dm04@group.calendar.google.com'
 all_event_ids = []
 
 
@@ -59,46 +60,23 @@ def delete_all_events(creds):
             # Delete each event from Calendar and event_id from list
             for event_id in trash_ids:
                 # Delete calendar event
-                service.events().delete(calendarId=CALENDER_ID, eventId=event_id).execute()
+                service.events().delete(calendarId=BLUE_CALENDER_ID, eventId=event_id).execute()
         all_event_ids.clear()
         os.remove('event_ids.json')
 
     except HttpError as error:
         print('An error occurred: %s' % error)
+    except FileNotFoundError:
+        print('No events to delete')
 
 
 def create_event(creds, event):
     try:
         service = build('calendar', 'v3', credentials=creds)
-        recurring_event = service.events().insert(calendarId=CALENDER_ID, body=event).execute()
+        recurring_event = service.events().insert(calendarId=BLUE_CALENDER_ID, body=event).execute()
 
         # Add event_id to all_event_ids when event is created
         all_event_ids.append(recurring_event['id'])
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
-
-
-def check_upcoming_events(creds):
-    try:
-        service = build('calendar', 'v3', credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
 
     except HttpError as error:
         print('An error occurred: %s' % error)
